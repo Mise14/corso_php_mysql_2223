c0684893b1807715918c4fac939ee55bd8a3600f
Fabio <ciaociao@gmail.com>
2023-03-27 08:49:33 +0200

    update user

--- form_in_php/index-user.php
+++ form_in_php/index-user.php
@@ -18,6 +18,7 @@ $users = (new UserCRUD())->read();
         <th>Nome</th>
         <th>Cognome</th>
         <th>Comune</th>
+        <th>Azioni</th>
     </tr>
     <?php foreach($users as $user){ ?>
     <tr>
@@ -25,6 +26,10 @@ $users = (new UserCRUD())->read();
         <td> <?= $user->first_name ?> </td>
         <td> <?= $user->last_name ?> </td>
         <td> <?= $user->birth_city ?> </td>
+        <td>
+          <a href="create-user.php" class="btn btn-primary btn-xs"> edit</a>
+          <a class="btn btn-primary btn-xs"> delete</a>
+        </td>
     </tr>
     <?php } ?>
 </table>

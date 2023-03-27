b90f6e529b4d4dab3068adabf474e3c42557d65b
Fabio <ciaociao@gmail.com>
2023-03-27 09:49:19 +0200

    test-update

--- form_in_php/test/crud/test_user_read.php
+++ form_in_php/test/crud/test_user_read.php
@@ -47,16 +47,39 @@ if(is_array($result) && count($result) === 1 ){
 };
 
 
-$crud->delete(1);
-$result = $crud->read(1);
-if($result === false){
-    echo "\nutente con id 1 è stato eliminato\n";
-}
+// $crud->delete(1);
+// $result = $crud->read(1);
+// if($result === false){
+//     echo "\nutente con id 1 è stato eliminato\n";
+// }
 
-// TODO: inserire un secondo utente per  testare la cancellazione singola
 
 var_dump($result);
 
+$user2 = new User();
+$user2->first_name = "Filippo";
+$user2->last_name = "Rosso";
+$user2->birth_city = "Torino";
+$user2->birthday = "2017-01-01";
+$user2->gender = "M";
+$user2->regione_id = 9;
+$user2->provincia_id = 15;
+$user2->username = "ciaociaociaociao@gmail.com";
+$user2->password = md5('Password');
 
+$result = $crud->update(1, $user2);
+if($result == 1){
+    echo "\nutente aggiornato\n";
+}elseif($result>1){
+    echo "\ntroppi utenti aggiornati";
+}elseif($result==0){
+    echo "\nnessun utente aggiornato";
+}
+
+
+$result = $crud->read(); //7 array | vuoto 
+if(is_array($result) && count($result) === 1 ){
+    echo "\nricerca di tutti gli utenti (1)\n";
+};
 
 

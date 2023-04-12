23ad321010d09597d6fd83d53a9707a401fa6b09
Fabio <ciaociao@gmail.com>
2023-04-12 12:45:10 +0200

    renderview

---
+++ javascript_http/RenderView.js
@@ -0,0 +1,44 @@
+
+//--------------------Funzione 1-------------------
+function UsersList(array_users, element_selector) {
+    //indico che la lista la voglio nell'index.html nell'id che indico quando richiamo la funzione
+    const lista = document.getElementById(element_selector)
+    //per ottenere l'elenco prendo il json -> entro dentro data -> li mappo (trasformo in qualcos altro)
+    const elenco = array_users.map((user) => {
+        //console.log("sono un utente",user)
+        return "<li>" + user.first_name + " " + user.last_name + "</li>"
+        //prendo gli elementi dell'array e li unisco con una stringa usando .join
+    }).join("")
+
+    lista.innerHTML = elenco
+}
+
+//--------------------Funzione 2-------------------
+//Chiamo una variabile a cui associo una funzione -> function expression
+const UserTable = (array_users, element_selector) => {
+
+    const html = `<table border="1" width="100%">
+        <tr>
+            <th>
+                Nome
+            </th>
+        </tr>
+`
+        +
+
+        //Prendo tutto l'array utenti -> con map ne prendo uno solo
+        array_users.map((user) => {
+            return `<tr>
+                <td> 
+                     ${user.first_name}
+                </td>
+            </tr>`
+        }).join("")
+        +
+        `</table>`
+    
+        document.getElementById(element_selector).innerHTML = html
+
+}
+
+export { UsersList, UserTable }
--- javascript_http/UserService.js
+++ javascript_http/UserService.js
@@ -9,28 +9,10 @@ const base_url = "http://localhost/corso_php_mysql_2223/form_in_php/rest_api"
 //per poterla usare
 export function getUser(){
 
-    //Utilizzando Fetch unisco l'ultima parte dell'url per fare la chiamata HTPP, è il client di HTPP di ECMA6
+    //Usiamo Fetch per fare la chiamata HTPP, è il client di HTPP di ECMA6
     //Promise(promessa) è un oggetto che ancora non contiene le informazioni, ma prima o poi arriveranno
-    const promise = fetch(base_url+"/users.php")
-    console.log("1 promessa di fetch",promise)
+    return fetch(base_url+"/users.php").then(response => response.json())
     //Then prende una callback, la funzione verrà eseguita quanto la promise verrà soddisfatta
-    promise.then((response) => {
         //.json trasforma la risposta in json
-        return response.json()
-    })
-    .then((json) => {
-        //visualizzo i dati disponibili sulla console
-        console.log(json)
-        //li stampo nell'index
-        const lista = document.getElementById("lista_utenti")
-        //per ottenere l'elenco prendo il json -> entro dentro data -> li mappo (trasformo in qualcos altro)
-        const elenco = json.map((user)=>{
-            console.log("sono un utente",user)
-            return "<li>"+user.first_name+"</li>"
-            //prendo gli elementi dell'array e li unisco con una stringa usando .join
-        }).join("")
-
-        lista.innerHTML = elenco
-    })
 
 }
